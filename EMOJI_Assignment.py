d43cf74e9faa833b20651f42fab6a0743143e0f0
dexter2470 <119699896+dexter2470@users.noreply.github.com>
2023-07-30 18:04:20 +0100

    Add files via upload

---
+++ EMOJI_Assignment.py
@@ -0,0 +1,31 @@
+message = input("> ")
+words = message.split( " ")
+output=""
+
+emojis= { "disappointed":'🤦‍♀️',
+         "clown":'🤡',
+         "smile":'😊',
+        "happy":"😀",
+         "sad":"☹️",
+         "lol" : "😂",
+         "sick":"😨",
+         "happy": "😀",
+         "pissed off":"🤬",
+         "I Hail ooo":'🙌',
+         "in love": '😍',
+         "love":"❤️",
+         "kiss":'😘',
+         "hug":'🤗',
+         "cool":'😎',
+         "okay":'👌',
+         "rotfl":'🤣',
+
+
+
+
+
+}
+
+for word in words:
+    output+= emojis.get(word, word) + " "
+    print(output)
